dae564ca8160b0b9ec8fa80b3b926ef4fbf61aab
Patrick Wang <patrick.bohan.wang@gmail.com>
2019-07-26 14:43:35 -0700

    debuggin

--- public/calibrate.js
+++ public/calibrate.js
@@ -2,7 +2,7 @@
 var socket = io('192.168.86.100:7000');
 
 document.addEventListener('keydown', (e) => {
-    console.log(e);
-    socket.emit('keydown', e);
+    console.log(e.key);
+    socket.emit('keydown', {key: e.key});
     console.log(e);
 });
